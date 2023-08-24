31bb40dccbc0e3320cd5b1386ed68fc131fc11c3
Rod97139 <fayaflame@gmail.com>
2023-08-24 06:05:57 +0200

    start project

---
+++ scripts/utils/contactForm.js
@@ -0,0 +1,9 @@
+const displayModal = () => {
+    const modal = document.getElementById("contact_modal");
+	modal.style.display = "block";
+}
+
+const closeModal = () => {
+    const modal = document.getElementById("contact_modal");
+    modal.style.display = "none";
+}
