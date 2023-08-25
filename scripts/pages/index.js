5e4c3fc81b8de831778671295ab2fa3ad723a126
Rod97139 <fayaflame@gmail.com>
2023-08-25 03:06:33 +0200

    saveToLocalStorage()

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,35 +1,43 @@
 import { photographerTemplate } from "../templates/photographer.js";
 
+    const saveToLocalStorage = async (data) => {
+        window.localStorage.setItem("photographers", JSON.stringify(data.photographers));
+        const photosByPhotographer = {} 
+        let lastId = null;
+        data.media.forEach((media) => {
+            if (!photosByPhotographer[media.photographerId]) {
+                if (lastId) {
+                    window.localStorage.setItem(`media-${lastId}`, JSON.stringify(photosByPhotographer[lastId])); 
+                }
+                photosByPhotographer[media.photographerId] = [];
+            }
+            photosByPhotographer[media.photographerId].push(media);
+            lastId = media.photographerId;
+            
+        });
+        window.localStorage.setItem(`media-${lastId}`, JSON.stringify(photosByPhotographer[lastId])); 
+    }
+
 
     const getPhotographers = async () => {
         // Ceci est un exemple de données pour avoir un affichage de photographes de test dès le démarrage du projet, 
         // mais il sera à remplacer avec une requête sur le fichier JSON en utilisant "fetch".
-        let photographers = [
-            {
-                "name": "Ma data test",
-                "id": 1,
-                "city": "Paris",
-                "country": "France",
-                "tagline": "Ceci est ma data test",
-                "price": 400,
-                "portrait": "account.png"
-            },
-            {
-                "name": "Autre data test",
-                "id": 2,
-                "city": "Londres",
-                "country": "UK",
-                "tagline": "Ceci est ma data test 2",
-                "price": 500,
-                "portrait": "account.png"
-            },
-        ]
+        
+        let photographers = window.localStorage.getItem("photographers");
+
+        if (!photographers) {
+            const data = await fetch("data/photographers.json").then(data => data.json());
+            saveToLocalStorage(data);
+            photographers = data.photographers;
+        } else {
+            photographers = JSON.parse(photographers);
+        }
         // et bien retourner le tableau photographers seulement une fois récupéré
         return ({
-            photographers: [...photographers, ...photographers, ...photographers]})
+            photographers: [...photographers]})
     }
 
-    const displayData = async (photographers) => {
+    const displayData = (photographers) => {
         const photographersSection = document.querySelector(".photographer_section");
 
         photographers.forEach((photographer) => {
