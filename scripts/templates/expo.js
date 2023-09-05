4e4a76c0c2730be2d902f0b27964998f2168ce98
Rod97139 <fayaflame@gmail.com>
2023-09-05 02:47:00 +0200

    contact form listener

--- scripts/templates/expo.js
+++ scripts/templates/expo.js
@@ -5,15 +5,31 @@ export const expoTemplate = (data, photographer) => {
     const firstName = myArray[0];
     const picture = `assets/media/${firstName}/${file}`;
 
-    const getUserCardDOM = () => {
+    const getExpoCardDOM = () => {
         const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = title;
-        article.appendChild(h2);
-        article.appendChild(img);
+
+        // The same code in the end of the function to fill the article in jsx:
+        article.innerHTML = `<img src="${picture}" alt="${title}">
+                                <h2>${title}</h2>`;
+                                
         return (article);
     }
-    return { title, picture, getUserCardDOM }
+    return { title, picture, getExpoCardDOM }
+}
+
+export const expoPhotographerTemplate = (data) => {
+    const { name, portrait } = data;
+
+    const picture = `assets/photographers/${portrait}`;
+
+    const getExpoPhotographerCardDOM = () => {
+        const article = document.createElement( 'article' );
+        article.innerHTML = `   <h1>${name}</h1>
+                                <p>${data.city}, ${data.country}</p>
+                                <p>${data.tagline}</p>
+                                <p>${data.price}€/jour</p>`;
+
+        return (article);
+    }
+    return { name, picture, getExpoPhotographerCardDOM }
 }
--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,25 @@
-const displayModal = () => {
+export const displayModal = () => {
+    document.body.insertAdjacentHTML('beforeend', createModal());
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
+
+    return modal
 }
 
-const closeModal = () => {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+export const createModal = () => {
+    return`<div id="contact_modal">
+			<div class="modal">
+			<header>
+                <h2>Contactez-moi</h2>
+                <img src="assets/icons/close.svg" />
+            </header>
+				<form>
+					<div>
+						<label>Prénom</label>
+						<input />
+					</div>
+                    <button class="contact_button">Envoyer</button>
+				</form>
+			</div>
+		</div>`
 }
