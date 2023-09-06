634ba75c38950f5ec81f571e976d4b1d01e635b3
Rod97139 <fayaflame@gmail.com>
2023-09-06 04:38:26 +0200

    modal contact

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,25 +1,22 @@
 export const displayModal = () => {
-    document.body.insertAdjacentHTML('beforeend', createModal());
+    document.body.insertAdjacentHTML('afterbegin', `<div id="contact_modal">
+														<div class="modal">
+														<header>
+															<h2>Contactez-moi</h2>
+															<img src="assets/icons/close.svg" />
+														</header>
+															<form>
+																<div>
+																	<label>Prénom</label>
+																	<input />
+																</div>
+																<button class="contact_button">Envoyer</button>
+															</form>
+														</div>
+													</div>`);
+
     const modal = document.getElementById("contact_modal");
 	modal.style.display = "block";
 
     return modal
-}
-
-export const createModal = () => {
-    return`<div id="contact_modal">
-			<div class="modal">
-			<header>
-                <h2>Contactez-moi</h2>
-                <img src="assets/icons/close.svg" />
-            </header>
-				<form>
-					<div>
-						<label>Prénom</label>
-						<input />
-					</div>
-                    <button class="contact_button">Envoyer</button>
-				</form>
-			</div>
-		</div>`
 }
