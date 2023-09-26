01cd78e46cc70624cae72d0d10c837b722c0ed96
Rod97139 <fayaflame@gmail.com>
2023-09-26 22:04:10 +0200

    lightbox

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,23 +1,23 @@
 export const displayLightbox = () => {
-	const lightbox = document.querySelector('#myLightbox')
-	if (lightbox) {
-		lightbox.style.display = 'block'
-		return lightbox
-	}
+    const lightbox = document.querySelector('#myLightbox')
+    if (lightbox) {
+      lightbox.style.display = 'block'
+      return lightbox
+    }
     const newLightbox = document.createElement("div");
-	newLightbox.id = "myLightbox";
-	const lightboxContent = document.createElement("div");
-	lightboxContent.classList.add("lightbox-content");
+    newLightbox.id = "myLightbox";
+    const lightboxContent = document.createElement("div");
+    lightboxContent.classList.add("lightbox-content");
     const prev = document.createElement("a");
     prev.classList.add("prev");
     prev.textContent = "prev";
     const next = document.createElement("a");
     next.classList.add("next");
-	newLightbox.appendChild(lightboxContent);
+    newLightbox.appendChild(lightboxContent);
     newLightbox.appendChild(prev);
     newLightbox.appendChild(next);
-	const $main = document.querySelector("main");
-	$main.append(newLightbox);
+    const $main = document.querySelector("main");
+    $main.append(newLightbox);
 
     // const $closeBtn = document.querySelector('.lightbox img')
     // $closeBtn.addEventListener('click', () => {
