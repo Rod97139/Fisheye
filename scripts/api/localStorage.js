b1125388e9a3c7d1e249011bbcc7b4e1ce2e2787
Rod97139 <fayaflame@gmail.com>
2024-02-08 06:27:19 +0100

    isLiked

--- scripts/api/localStorage.js
+++ scripts/api/localStorage.js
@@ -3,6 +3,7 @@ export const saveToLocalStorage = async (data) => {
     const photosByPhotographer = {} 
     let lastId = null;
     data.media.forEach((media) => {
+    media.isliked = 0;
         if (!photosByPhotographer[media.photographerId]) {
             if (lastId) {
                 window.localStorage.setItem(`media-${lastId}`, JSON.stringify(photosByPhotographer[lastId])); 
